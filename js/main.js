e66e64c03f5ff93621ea39a00fcb467e47f2ad37
Maxwell Silverstein <mrs84@duke.edu>
2019-07-17 13:58:09 +0300

    barcelona

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWF4d2VscyIsImEiOiJjank3MDd1ZmMwbTBhM2dxbnUza
 var map = new mapboxgl.Map({
 container: 'map', // container id
 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [34.7818, 32.0853], // starting position [lng, lat]
+center: [2.1734, 41.3851], // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
